16fc7c3f8c31f37c30009f24a195698165612f12
Marcos Gabriel <marcosgabriel166@gmail.com>
2025-09-12 22:22:58 -0300

    :sparkles: feat: demonstra erros de tipo em ambiente Typescript

---
+++ questao_09.ts
@@ -0,0 +1,13 @@
+// Exemplo 1
+let a_ts = 10;
+let b_ts = "5";
+console.log(a_ts + b_ts);
+
+// O operador "+" não pode ser aplicado aos tipos NUMBER e STRING
+
+// Exemplo 2
+let x_ts = true;
+let y_ts = 2;
+console.log(x_ts + y_ts);
+
+// O operador "+" não pode ser aplicado aos tipos BOOLEAN e NUMBER

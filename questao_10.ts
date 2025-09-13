77c4abc8f112eabcc2d665ba559f12dff823b566
Marcos Gabriel <marcosgabriel166@gmail.com>
2025-09-12 22:23:18 -0300

    :sparkles: feat: implementa uso de template strings para formatação

---
+++ questao_10.ts
@@ -0,0 +1,10 @@
+const nome = "Ely";
+const salario = 2000;
+const linguagem = "Typescript";
+
+const mensagemQuestao = `${nome}
+My payment time is ${salario.toFixed(2)}
+and
+my preffered language is ${linguagem}`;
+
+console.log(mensagemQuestao);

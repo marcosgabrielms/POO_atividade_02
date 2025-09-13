3dd36b2a437a48f8600b68fdb27a843a0f69c6ef
Marcos Gabriel <marcosgabriel166@gmail.com>
2025-09-12 22:25:27 -0300

    :sparkles: feat: adiciona exemplo para a regra NoImplicitAny

---
+++ questao_17.ts
@@ -0,0 +1,6 @@
+let valor; // <-- Problema aqui
+valor = 10;
+valor = "teste";
+console.log(valor);
+
+// Resposta: Com a regra ativa o compilador dá um erro; com a regra desativada ele compila com sucesso.
